33c27ea18e223b11d98725459177468bb03b52ae
Ryan O'Connor <72693746+TheOneTrueNoodle@users.noreply.github.com>
2022-09-24 11:53:49 +0100

    First commit

--- GE1 Examples 2022/Assets/Cube1.cs
+++ GE1 Examples 2022/Assets/Cube1.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Cube1 : MonoBehaviour
 {
+    [SerializeField] private int x_speed;
+    [SerializeField] private int y_speed;
+    [SerializeField] private int z_speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class Cube1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0, 1);
+        transform.Translate(x_speed, y_speed, z_speed);
     }
 }
