7756f58a18095964c4351e48cfa0d54281b77c36
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2022-10-05 13:27:17 +0100

    Made the generator and rotation scripts
    Still need to add color to the gameobjects but the generation works!

--- GE1 Examples 2022/Assets/Generator.cs
+++ GE1 Examples 2022/Assets/Generator.cs
@@ -6,14 +6,49 @@ public class Generator : MonoBehaviour
 {
     public int loops = 10;
     public GameObject prefab;
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] int radius = 2;
+    
+
+    private void Start()
     {
+        //Repeat this for each loop and have it be further out for each loop...
+        for(int i = 0; i < loops; i++)
+        {
+            int circumference = (int)(2 * Mathf.PI * (i * radius));
+            
+            //Then we need another loop to create each individual dodecahedron
+            for(int j = 0; j < circumference; j++)
+            {
+                //How do i calculate where on the circle it spawns.......
+                float cx = gameObject.transform.position.x;
+                float cy = gameObject.transform.position.y;
+
+                float a = j * ((2f * Mathf.PI) / circumference);
+
+                float x = cx + (radius * i) * Mathf.Cos(a);
+                float y = cy + (radius * i) * Mathf.Sin(a);
+
+                GameObject newObj = Instantiate(prefab);
+                newObj.transform.position = new Vector3(x, y, 0);
+
+                newObj.transform.parent = gameObject.transform;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
-    }
+    /* IM PUTTING ALL NOTES IN HERE SO I DONT CLUTTER CODE
+     * If we have the circumferance of the circle, which represents the length of the circles line, we can use that number to get the number of required dodecahedrons for this loop!!!
+     * Then add the i from loop to make it further out each time...
+     * 
+     * Time for MATH!!!!!
+     * How to calculate the coordinates of a point on the curcumference of a circle!!!!
+     * x = cx + r * cos(a)
+     * y = cy + r * sin(a)
+     * Where r is the radius, (cx,cy) is the orogin of the circle and a is the angle
+     * 
+     * So time to get the two things!!!!
+     * To get the angle - If we assume the first one should be at angle 0, then we can simply get the angle each time based on the loop, by dividing 360 by the number of dodecahedrons to be instantiated. Total angle in radians is 2pi
+     */
 }
--- GE1 Examples 2022/Assets/RotateMe.cs
+++ GE1 Examples 2022/Assets/RotateMe.cs
@@ -6,14 +6,9 @@ public class RotateMe : MonoBehaviour
 {
     [Range(0, 360)]
     public float speed = 90;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
     }
 }
