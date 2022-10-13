5c9b36d8ca7c7225525b628e0aa0425092453e39
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2022-10-13 19:36:29 +0100

    Functioning Rotation

---
+++ GE1 Examples 2022/Assets/Fish.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    public Transform HeadTransform;
+    public Transform TailTransform;
+    [Range(0.0f, 5.0f)]
+    public float Frequency;
+    public float HeadAmplitude = 40f;
+    public float TailAmplitude = 40f;
+
+    public float Theta = 0;
+
+    private void Start()
+    {
+        Theta = 0;
+    }
+
+
+    public void Update()
+    {
+        HeadTransform.localRotation = Quaternion.AngleAxis(HeadAmplitude * Mathf.Sin(Time.time * Frequency), Vector3.forward);
+        TailTransform.localRotation = Quaternion.AngleAxis(TailAmplitude * Mathf.Sin(Time.time * Frequency), Vector3.forward);
+    }
+}
